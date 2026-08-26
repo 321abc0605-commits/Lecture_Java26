9e8018fccf2d667e2b5705b597b8f699cc8ee993
321abc0605-commits <321abc0605@gmail.com>
2026-08-26 17:59:46 +0900

    Add files via upload

---
+++ day02/src/day02/Homework06.java
@@ -0,0 +1,14 @@
+package day02;
+
+import java.util.Scanner;
+
+public class Homework06 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        int F = sc.nextInt();
+        System.out.println((5d/9) * (F - 32));
+        
+	}
+
+}
